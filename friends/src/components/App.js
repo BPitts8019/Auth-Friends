c82e24477169f284b17f986222b0dab985eea69a
BPitts8019 <bradleyj.pitts@gmail.com>
2019-11-01 17:09:17 -0700

    Added Less to workflow with basic styling

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
 
 function App() {
    return (
       <div className="App">
-
+         <header>
+            <h1>My Friends List</h1>
+            <nav>
+               <a>Hello</a>
+               <a>World</a>
+            </nav>
+         </header>
       </div>
    );
 }
